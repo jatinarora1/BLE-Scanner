d18d86b620ed00ee6b94d06343abbd80675bc047
Jatin Arora <arora8331@gmail.com>
2021-02-23 16:42:36 +0530

    v1

---
+++ app/src/main/java/com/application/blesample/extensions/View.kt
@@ -0,0 +1,7 @@
+package com.application.blesample.extensions
+
+import android.view.View
+
+fun View.visible() { this.visibility = View.VISIBLE }
+
+fun View.invisible() { this.visibility = View.GONE }
---
+++ app/src/main/java/com/application/blesample/extensions/ViewGroup.kt
@@ -0,0 +1,11 @@
+package com.application.blesample.extensions
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+fun ViewGroup.inflate(layoutRes: Int): View = LayoutInflater.from(context).inflate(
+    layoutRes,
+    this,
+    false
+)
